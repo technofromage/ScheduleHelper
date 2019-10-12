f4f13c1c15495f9a171e9677242c21f212559052
Caleb <caleb.hooper@wsu.edu>
2019-10-11 21:47:13 -0700

    Added Base Project Files

---
+++ Login/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+
+    <link rel="stylesheet" href="./login.css">
+
+    <body id="body">
+
+        <div id="container">
+
+            <div id="list">
+
+                <div id="element" class="Title">Login</div>
+
+                <div id="spacer"> </div>
+
+                <div id="element">User Name: <Input type="text"> </div> 
+
+                <div id="element">Password: <Input type="text"> </div> 
+
+                <input id="element" class="Button" type="button" value="Submit">
+            
+            </div>
+
+        </div>
+
+    </body>
+
+
+</html>
